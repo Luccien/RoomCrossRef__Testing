638439b972afd6f631d8b1fdfc4f2944797e8e32
Your Name <you@example.com>
2020-12-16 08:50:54 +0100

    id is missing error - test with no outogenerated

--- app/src/main/java/eu/de/servicetestfragment/framework/db/profileWithNotes/ProfileWithNotesEntity.kt
+++ app/src/main/java/eu/de/servicetestfragment/framework/db/profileWithNotes/ProfileWithNotesEntity.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.Junction
 import androidx.room.Relation
 import eu.de.core.data.note.Note
+import eu.de.core.data.noteProfileCrossRef.NoteProfileCrossRef
 import eu.de.core.data.profile.Profile
 import eu.de.servicetestfragment.framework.db.note.NoteEntity
 import eu.de.servicetestfragment.framework.db.noteProfileCrossRef.NoteProfileCrossRefEntity
@@ -12,13 +13,15 @@ import eu.de.servicetestfragment.framework.db.profile.ProfileEntity
 
 @Entity(tableName = "profileWithNotes")
 data class ProfileWithNotesEntity (
-    @Embedded val profileEntity: ProfileEntity,
+    @Embedded val profile: Profile,
     @Relation(
     parentColumn = "profileId",
     entityColumn = "noteId",
  associateBy = Junction(NoteProfileCrossRefEntity::class)
+        //associateBy = Junction(NoteProfileCrossRef::class)
+
     )
-        val notes: List<NoteEntity>
+        val notes: List<Note>
 )
 {
 
